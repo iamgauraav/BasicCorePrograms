c261b39a2475cb6fd7881e202c96a091c3778e46
iamgauraav <iamgauraav@gmail.com>
2022-03-12 23:30:34 +0530

    Added UC2 : Checking year whether it is leap year or not

--- Program.cs
+++ Program.cs
@@ -2,41 +2,25 @@
 
 namespace BasicCorePrograms
 {/// <summary>
-/// UC-1 Flip Coin and print percentage of Heads and Tails
+/// UC-2 Leap Year
 /// </summary>
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter number of times Flip the coin");   
-            int noFlip=int.Parse(Console.ReadLine());                   //Taking value from user
+            Console.WriteLine("Enter year..!!");        
+            int year=int.Parse(Console.ReadLine());
 
-            int head = 0;
-            int tail = 0;
-            Random random = new Random();                               //Random class
-
-            for (int i = 1; i < noFlip; i++)                            //for checking number of flip
+            //If-else selection statement
+            if((year % 4 ==0) && (year % 100 != 0) || (year % 400 ==0))     //checking for leap year 
             {
-                int ran = random.Next(0, 2);                            //next method (0-initial,1-final)
-                {
-                    if (ran == 0)
-                    {
-                        tail++;
-                    }
-                    else
-                    {
-                        head++;
-                    }
-
-                }
+                Console.WriteLine("{0} is a leap year : " , year);
             }
-            Console.WriteLine("Number of times Head occurs : "+ head);
-            Console.WriteLine("Number of times Tail occurs : "+ tail);
-            double percentageHead = (double)head / noFlip * 100;            //percentage of head
-            Console.WriteLine("Head percentage is : " + percentageHead);
-            double percentageTail = (double)tail / noFlip * 100;            //percentage of tail
-            Console.WriteLine("Tail percentage is : " + percentageTail);
+            else
+            {
+                Console.WriteLine("{0} is not leap year : " , year);
+            }
             Console.ReadLine();
         }
     }
